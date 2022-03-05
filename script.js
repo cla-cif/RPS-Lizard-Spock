5d8afe74ccfe9f826075d256ff71dd9cabbfd067
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-03-05 08:45:55 +0000

    test js

---
+++ script.js
@@ -0,0 +1,5 @@
+//wait for the DOM to finish loading before executing functions
+document.addEventListener("DOMContentLoaded", function() {
+    let choiches = getElementsbyClassName("uchoiche");
+})
+choices.addEventListener('click', function () {alert('hi')}
