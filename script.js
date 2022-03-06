f60e0bab3bde4597b159c9e50391ab14339c0524
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-03-06 10:59:47 +0000

    add game rules

--- script.js
+++ script.js
@@ -2,26 +2,53 @@ document.addEventListener("DOMContentLoaded", function () {
     let choices = document.getElementsByClassName("uchoice");
     for (let choice of choices) {
         choice.addEventListener("click", function () {
-            incrementScore();
             computerChoice();
 
         });
     }
 });
 
-// function outRock() {
-//     var rock = document.getElementById("crock");
-//     rock.style.fill = "#ffd700";
-// };
+const selectWinner = (user, computer) => {
+    let result = null;
+  
+    const ifVal = (a, b, w) =>
+      user.choice === a && computer.choice === b ? (result = w) : null;
+  
+    ifVal("rock", "scissors", user);
+    ifVal("rock", "lizard", user);
+    ifVal("paper", "rock", user);
+    ifVal("paper", "spock", user);
+    ifVal("scissors", "paper", user);
+    ifVal("scissors", "lizard", user);
+    ifVal("lizard", "spock", user);
+    ifVal("lizard", "paper", user);
+    ifVal("spock","rock", user);
+    ifVal("spock", "scissors", user);
 
-function incrementScore() {
-    // Gets the current score from the DOM and increments it
-    let userScore = parseInt(document.getElementById("you-score").innerText);
-    document.getElementById("you-score").innerText = ++userScore;
+    ifVal("rock", "paper", computer);
+    ifVal("rock", "spock", computer);
+    ifVal("paper", "lizard", computer);
+    ifVal("paper", "scissors", computer);
+    ifVal("scissors", "spock", computer);
+    ifVal("scissors", "rock", computer);
+    ifVal("lizard", "rock", computer);
+    ifVal("lizard", "scissor", computer);
+    ifVal("spock", "lizard", computer);
+    ifVal("spock", "paper", computer);
+
+    return result;
+};
+
+
+function incrementComputerScore() {    
     let computerScore = parseInt(document.getElementById("computer-score").innerText);
     document.getElementById("computer-score").innerText = ++computerScore;
 }
 
+function incrementUserScore () {
+    let userScore = parseInt(document.getElementById("you-score").innerText);
+    document.getElementById("you-score").innerText = ++userScore;
+}
 
 function resetScore() {
     let clear = document.getElementById("reset");
@@ -34,22 +61,24 @@ resetScore();
 
 function computerChoice() {
     const rand = Math.floor(Math.random() * 5) + 1;
-    alert(`Computer chose: ${rand}`);
-    let rock = document.getElementById("crock");
-    let paper = document.getElementById("cpaper");
-    let scissor = document.getElementById("cscissor");
-    let lizard = document.getElementById("clizard");
-    let spock = document.getElementById("cspock");
-
+    let rock = document.getElementById("path-rock");
+    let paper = document.getElementById("path-paper");
+    let scissor = document.getElementById("path-scissor");
+    let lizard = document.getElementById("path-lizard");
+    let spock = document.getElementById("path-spock");
     if (rand == 1) {
         rock.style.fill = "#ffd700";
     } else if (rand == 2) {
         paper.style.fill = "#ffd700";
+        paper.style.transitionDuration= "2s";
     } else if (rand == 3) {
         scissor.style.fill = "#ffd700";
+        scissor.style.transitionDuration= "2s";
     } else if (rand == 4) {
         lizard.style.fill = "#ffd700";
+        lizard.style.transitionDuration= "2s";
     } else if (rand == 5) {
         spock.style.fill = "#ffd700";
+        spock.style.transitionDuration= "2s";
     }
 }
