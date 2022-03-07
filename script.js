3742481fd1593c025376803a875e0f07e0c0187f
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-03-07 11:58:23 +0000

    add result message

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const reset_btn = document.getElementById("reset");
 const rock_div = document.getElementById("urock");
 const paper_div = document.getElementById("upaper");
 const scissors_div = document.getElementById("uscissors");
 const lizard_div = document.getElementById("ulizard");
 const spock_div = document.getElementById("uspock");
+const result_p = document.getElementById("result-message");
 
 /*document.addEventListener("DOMContentLoaded", function () {
     let choices = document.getElementsByClassName("uchoice");
@@ -41,47 +46,53 @@ function game(uChoice) {
         case "rocklizard":
         case "paperrock":
         case "paperspock":
-        case "scissorsspaper":
-        case "scissorsslizard":
+        case "scissorspaper":
+        case "scissorslizard":
         case "lizardspock":
         case "lizardpaper":
         case "spockrock":
         case "spockscissors":
             incrementUserScore();
-            alert("you win");
+
             break;
         case "rockpaper":
         case "rockspock":
         case "paperlizard":
         case "paperscissors":
-        case "scissorssspock":
-        case "scissorssrock":
+        case "scissorsspock":
+        case "scissorsrock":
         case "lizardrock":
         case "lizardscissors":
         case "spocklizard":
         case "spockpaper":
-            alert("computer win");
+
             incrementComputerScore()
             break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+        case "lizardlizard":
+        case "spockspock":
+
     }
 }
 
 
-
 function incrementComputerScore() {
-    let computerScore = parseInt(document.getElementById("computer-score").innerText);
-    document.getElementById("computer-score").innerText = ++computerScore;
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
 }
 
 function incrementUserScore() {
-    let userScore = parseInt(document.getElementById("user-score").innerText);
-    document.getElementById("user-score").innerText = ++userScore;
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_p.innerHTM= "computer won";
 }
 
 function resetScore() {
     reset_btn.addEventListener("click", function () {
-        document.getElementById("user-score").innerText = 0;
-        document.getElementById("computer-score").innerText = 0;
+        computerScore_span.innerHTML = 0;
+        userScore_span.innerHTML = 0;
     });
 }
 resetScore();
@@ -89,23 +100,23 @@ resetScore();
 function computerChoice() {
     const computerDraws = ["rock", "paper", "scissors", "lizard", "spock"]
     const randomNumber = Math.floor(Math.random() * 5) + 1;
-    return computerDraws[randomNumber]
-    /* if (cChoices === "rock") {
-         document.getElementById("path-rock").style.fill = "#ffd700";
-         
-     } else if (randomNumber == 2) {
-         document.getElementById("path-paper").style.fill = "#ffd700";
-       
-     } else if (randomNumber == 3) {
-         document.getElementById("path-scissors").style.fill = "#ffd700";
-         
-     } else if (randomNumber == 4) {
-         document.getElementById("path-lizard").style.fill = "#ffd700";
-      
-     } else if (randomNumber == 5) {
-         document.getElementById("path-spock").style.fill = "#ffd700";
-      
-     } */
+
+    if (computerDraws[1]) {
+        document.getElementById("path-rock").style.fill = "#ffd700";
+
+    } else if (computerDraws[2]) {
+        document.getElementById("path-paper").style.fill = "#ffd700";
+
+    } else if (computerDraws[3]) {
+        document.getElementById("path-scissors").style.fill = "#ffd700";
+
+    } else if (computerDraws[4]) {
+        document.getElementById("path-lizard").style.fill = "#ffd700";
+
+    } else if (computerDraws[5]) {
+        document.getElementById("path-spock").style.fill = "#ffd700";
+    }
+    return computerDraws[randomNumber];
 }
 
 
