b6d07d0bc424693a7f92b523f7496323c16368f7
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-03-07 08:48:27 +0000

    added switch function

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const reset_btn = document.getElementById("reset");
 const rock_div = document.getElementById("urock");
 const paper_div = document.getElementById("upaper");
@@ -6,7 +5,7 @@ const scissors_div = document.getElementById("uscissors");
 const lizard_div = document.getElementById("ulizard");
 const spock_div = document.getElementById("uspock");
 
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     let choices = document.getElementsByClassName("uchoice");
     for (let choice of choices) {
         choice.addEventListener("click", function () {
@@ -15,15 +14,66 @@ document.addEventListener("DOMContentLoaded", function () {
             incrementUserScore();
         });
     }
-});
+});*/
+function main() {
+    rock_div.addEventListener("click", function () {
+        game("rock");
+    })
+    paper_div.addEventListener("click", function () {
+        game("paper");
+    })
+    scissors_div.addEventListener("click", function () {
+        game("scissors");
+    })
+    lizard_div.addEventListener("click", function () {
+        game("lizard");
+    })
+    spock_div.addEventListener("click", function () {
+        game("spock");
+    })
+};
+main();
+
+function game(uChoice) {
+    const cChoice = computerChoice();
+    switch (uChoice + cChoice) {
+        case "rockscissors":
+        case "rocklizard":
+        case "paperrock":
+        case "paperspock":
+        case "scissorsspaper":
+        case "scissorsslizard":
+        case "lizardspock":
+        case "lizardpaper":
+        case "spockrock":
+        case "spockscissors":
+            incrementUserScore();
+            alert("you win");
+            break;
+        case "rockpaper":
+        case "rockspock":
+        case "paperlizard":
+        case "paperscissors":
+        case "scissorssspock":
+        case "scissorssrock":
+        case "lizardrock":
+        case "lizardscissors":
+        case "spocklizard":
+        case "spockpaper":
+            alert("computer win");
+            incrementComputerScore()
+            break;
+    }
+}
 
 
-function incrementComputerScore() {    
+
+function incrementComputerScore() {
     let computerScore = parseInt(document.getElementById("computer-score").innerText);
     document.getElementById("computer-score").innerText = ++computerScore;
 }
 
-function incrementUserScore () {
+function incrementUserScore() {
     let userScore = parseInt(document.getElementById("user-score").innerText);
     document.getElementById("user-score").innerText = ++userScore;
 }
@@ -37,56 +87,36 @@ function resetScore() {
 resetScore();
 
 function computerChoice() {
-    const cChoices =["rock", "paper", "scissors", "lizard", "spock"]
+    const computerDraws = ["rock", "paper", "scissors", "lizard", "spock"]
     const randomNumber = Math.floor(Math.random() * 5) + 1;
-    return cChoices[randomNumber]
-    if (cChoices === "rock") {
-        document.getElementById("path-rock").style.fill = "#ffd700";
-        
-    } else if (randomNumber == 2) {
-        document.getElementById("path-paper").style.fill = "#ffd700";
+    return computerDraws[randomNumber]
+    /* if (cChoices === "rock") {
+         document.getElementById("path-rock").style.fill = "#ffd700";
+         
+     } else if (randomNumber == 2) {
+         document.getElementById("path-paper").style.fill = "#ffd700";
+       
+     } else if (randomNumber == 3) {
+         document.getElementById("path-scissors").style.fill = "#ffd700";
+         
+     } else if (randomNumber == 4) {
+         document.getElementById("path-lizard").style.fill = "#ffd700";
       
-    } else if (randomNumber == 3) {
-        document.getElementById("path-scissor").style.fill = "#ffd700";
-        r
-    } else if (randomNumber == 4) {
-        document.getElementById("path-lizard").style.fill = "#ffd700";
-     
-    } else if (randomNumber == 5) {
-        document.getElementById("path-spock").style.fill = "#ffd700";
-     
-    }
+     } else if (randomNumber == 5) {
+         document.getElementById("path-spock").style.fill = "#ffd700";
+      
+     } */
 }
-console.log(computerChoice());
+
 
 
 const selectWinner = (user, computer) => {
     let result = null;
-  
-    const ifVal = (a, b, w) =>
-      user.choice === a && computer.choice === b ? (result = w) : null;
-  
-    ifVal("rock", "scissors", user);
-    ifVal("rock", "lizard", user);
-    ifVal("paper", "rock", user);
-    ifVal("paper", "spock", user);
-    ifVal("scissors", "paper", user);
-    ifVal("scissors", "lizard", user);
-    ifVal("lizard", "spock", user);
-    ifVal("lizard", "paper", user);
-    ifVal("spock","rock", user);
-    ifVal("spock", "scissors", user);
 
-    ifVal("rock", "paper", computer);
-    ifVal("rock", "spock", computer);
-    ifVal("paper", "lizard", computer);
-    ifVal("paper", "scissors", computer);
-    ifVal("scissors", "spock", computer);
-    ifVal("scissors", "rock", computer);
-    ifVal("lizard", "rock", computer);
-    ifVal("lizard", "scissor", computer);
-    ifVal("spock", "lizard", computer);
-    ifVal("spock", "paper", computer);
+    const ifVal = (a, b, w) =>
+        user.choice === a && computer.choice === b ? (result = w) : null;
+
+
 
     return result;
 };
