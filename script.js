7921e52ff50cfbf3d2e0b23c6f71baa4f4ab3464
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-03-09 11:33:45 +0000

    main function runs afterDOM loaded

--- script.js
+++ script.js
@@ -13,35 +13,27 @@ const scissors_div = document.getElementById("uscissors");
 const lizard_div = document.getElementById("ulizard");
 const spock_div = document.getElementById("uspock");
 
-/*document.addEventListener("DOMContentLoaded", function () {}); NOT SURE HOW TO USE THIS*/
-
-function main() {
-    //pass to the game funcion the user's choice on click and makes the result message visible
+document.addEventListener("DOMContentLoaded", function () {
     rock_div.addEventListener("click", function () {
         game("rock");
-        result_p.style.visibility = "visible";
     })
     paper_div.addEventListener("click", function () {
         game("paper");
-        result_p.style.visibility = "visible";
     })
     scissors_div.addEventListener("click", function () {
         game("scissors");
-        result_p.style.visibility = "visible";
     })
     lizard_div.addEventListener("click", function () {
         game("lizard");
-        result_p.style.visibility = "visible";
     })
     spock_div.addEventListener("click", function () {
         game("spock");
-        result_p.style.visibility = "visible";
     })
-};
-main();
+}); 
 
 function game(uChoice) {
     //takes as an argument the user's choice and compares it with the random computer choice
+    result_p.style.visibility = "visible";
     const cChoice = computerChoice();
     switch (uChoice + cChoice) {
         case "rockscissors":
@@ -83,7 +75,10 @@ function game(uChoice) {
 function tie() {
     //displays a temporary message with the outcome of the game function
     result_p.innerHTML = "IT'S A TIE!"
-    setTimeout(() => (result_p.style.visibility = "hidden"), 1500);
+    setTimeout(() => (result_p.style.visibility = "hidden"), 1500); 
+    /* IF CLICK AGAIN I WANT TO END TIMEOUT
+     $(document).on("click",function(){
+    clearTimeout(timer);*/
 }
 
 function incrementComputerScore() {
@@ -105,12 +100,11 @@ function incrementUserScore() {
 /* WHAT I'M WORKING ON*/
 /*function incrementComputerScore() {
     //displays a temporary message with the outcome of the game function and increments the score
-    while (computerScore<10)  {
         computerScore++;
         computerScore_span.innerHTML = computerScore;
         result_p.innerHTML = "COMPUTER WON!";
         setTimeout(() => (result_p.style.visibility = "hidden"), 1500);
-        if (computerScore = 10) {
+        if (computerScore == 10) {
             result_p.innerHTML = "COMPUTER WON THE MATCH!"; 
             //or better call win and reset functions//
         }
@@ -119,14 +113,15 @@ function incrementUserScore() {
 
 function winner(userScore, computerScore) {
     // IT DOESN'T WORK! - displays the winner and resets the game
-    if (computerScore == "10") {
+    if (computerScore == 10) {
         result_p.innerHTML = "COMPUTER WON THE MATCH!";
         setTimeout(() => (result_p.style.visibility = "hidden"), 1500);
     }
-    if (userScore == "10") {
+    if (userScore == 10) {
         result_p.innerHTML = "YOU WON THE MATCH!";
         setTimeout(() => (result_p.style.visibility = "hidden"), 1500);
     }
+    resetScore();
 }
 winner();
 
